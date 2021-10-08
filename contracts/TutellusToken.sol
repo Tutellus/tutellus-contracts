2243339dadb77ca972c526ffcaff35dcdad47115
victormer <victormerinokosina@protonmail.com>
2021-10-08 10:40:08 +0200

    new infrastructure

--- contracts/TutellusToken.sol
+++ contracts/TutellusToken.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.0;
 import "./utils/Token.sol";
 
 contract TutellusToken is Token {
-    constructor() Token('Tutellus Token', 'TUT', 2e26) {}
+    constructor() Token('Tutellus Token', 'TUT', 2e26) {
+        // mint 400k to treasury
+    }
 }
