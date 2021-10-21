f46f93b844dcb2c9a0f9960c82ef40a6a710c537
victormer <victormerinokosina@protonmail.com>
2021-10-21 16:17:56 +0200

    add(updateClients) script for getting url and merkleroot

---
+++ scripts/updateClients.js
@@ -0,0 +1,29 @@
+const { create: ipfsHttpClient } = require('ipfs-http-client')
+const { readFile } = require('fs/promises')
+const { getBalanceTree } = require('../utils/balanceTree')
+const path = require('path')
+const jsonPath = '../examples/clients.json'
+const json = require(jsonPath)
+
+const data = {
+  host: 'ipfs.infura.io',
+  port: 5001,
+  protocol: 'https'
+}
+
+const main = async () => {
+  const ipfs = ipfsHttpClient(data);
+  try {
+    const file = await readFile(path.join(__dirname, jsonPath), 'utf8')
+    const tree = getBalanceTree(json)
+    const added = await ipfs.add(file)
+    console.log('Uri: https://ipfs.io/ipfs/' + added.cid.toString())
+    console.log('MerkleRoot: ', tree.toJSON().merkleRoot)
+  } catch(error) {
+    console.error('ERROR', error)
+  }
+}
+
+main();
+
+
