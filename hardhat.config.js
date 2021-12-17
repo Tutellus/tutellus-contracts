d2d5c33a0688f2c55707d2ee87ce5f7d35b74ef3
gperezalba <gperezalba94@gmail.com>
2021-12-17 13:33:15 +0100

    feat(hardhat.config.js) include forking mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -113,6 +113,11 @@ module.exports = {
       gasPrice: GAS_PRICE_DEFAULT,
       gasMultiplier: GAS_MULTIPLIER_DEFAULT,
       accounts: { mnemonic: process.env.MNEMONIC }
+    },
+    hardhat: {
+      forking: {
+        url: 'https://polygon-mainnet.g.alchemy.com/v2/' + process.env.ALCHEMY_API_KEY
+      }
     }
   },
   // This is a sample solc configuration that specifies which version of solc to use
