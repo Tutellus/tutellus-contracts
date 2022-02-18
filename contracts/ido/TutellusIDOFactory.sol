39b37d6ef9f5d183b2fcd3855569fef00d2f72de
gperezalba <gperezalba94@gmail.com>
2022-02-18 16:13:06 +0100

    feat(all) ido first approach

--- contracts/ido/TutellusIDOFactory.sol
+++ contracts/ido/TutellusIDOFactory.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
-import "../utils/AccessControlProxyPausable.sol";
+import "../utils/UUPSUpgradeableByRole.sol";
 import "./TutellusIDO.sol";
 
-contract TutellusIDOFactory is AccessControlProxyPausable{
+contract TutellusIDOFactory is UUPSUpgradeableByRole{
     address public beacon;
 
-    event NewIDO(address indexed proxy, address token, uint fundingAmount, uint minPrefundSuper, uint minPrefund);
+    event NewIDO(address indexed proxy, address token, uint fundingAmount, uint minPrefund);
 
     function initialize() public initializer {
         __AccessControlProxyPausable_init(msg.sender);
@@ -32,10 +32,8 @@ contract TutellusIDOFactory is AccessControlProxyPausable{
 
         proxyAddress = address(proxy);
 
-        (address token_,uint fundingAmount_,uint minPrefundSuper_, uint minPrefund_) = abi.decode(initializeCalldata[4:], (address, uint, uint, uint));
+        (,address token_,uint fundingAmount_, uint minPrefund_) = abi.decode(initializeCalldata[4:], (address, address, uint, uint));
 
-        _registerDID(owner, proxyAddress);
-
-        emit NewIDO(proxyAddress, token_, fundingAmount_, minPrefundSuper_, minPrefund_);
+        emit NewIDO(proxyAddress, token_, fundingAmount_, minPrefund_);
     }
 }
