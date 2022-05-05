06a4abccbf10f67efcf3190aa71d11a1eec7e66b
victormer <victormerinokosina@protonmail.com>
2022-05-05 18:08:59 +0200

    docs TutellusManager

--- contracts/interfaces/ITutellusManager.sol
+++ contracts/interfaces/ITutellusManager.sol
@@ -1,9 +1,91 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity 0.8.9;
 
+/// @title The interface of TutellusManager
+/// @dev Manages smart contracts deployments, ids and protocol roles
 interface ITutellusManager {
-    function addr(bytes32) external view returns (address);
-    function get(bytes32) external view returns (address);
-    function name(address) external view returns (bytes32);
-    function setId(bytes32, address) external;
+
+    /** EVENTS */
+
+    /// @dev Emitted when a new link is set between id and addr
+    /// @param id Hashed identifier linked to proxy
+    /// @param proxy Proxy contract address
+    /// @param implementation Implementation contract address
+    /// @param upgrade Flag: true when the proxy implementation is upgraded
+    event Deployment(
+        bytes32 indexed id,
+        address indexed proxy,
+        address indexed implementation,
+        bool upgrade
+    );
+
+    /// @dev Emitted when an identifier is locked forever to an address
+    /// @param id Hashed identifier linked to addr
+    /// @param addr Address linked to id
+    event Locked(
+        bytes32 indexed id,
+        address indexed addr
+    );
+
+    /// @dev Emitted when a new link is set between id and addr
+    /// @param id Hashed identifier linked to addr
+    /// @param addr Address linked to id
+    event NewId(
+        bytes32 indexed id,
+        address indexed addr
+    );
+
+    /** METHODS */
+
+    /// @dev Deploys / upgrades a proxy contract by deploying a new implementation
+    /// @param id Hashed identifier linked to the proxy contract
+    /// @param bytecode Bytecode for the new implementation
+    /// @param initializeCalldata Calldata for the initialization of the new contract (if necessary)
+    /// @return implementation Address of the new implementation
+    function deploy(
+        bytes32 id,
+        bytes memory bytecode,
+        bytes memory initializeCalldata
+    ) external returns ( address implementation );
+
+    /// @dev Deploys / overwrites a proxy contract with an existing implementation 
+    /// @param id Hashed identifier linked to the proxy contract
+    /// @param implementation Address of the existing implementation contract
+    /// @param initializeCalldata Calldata for the initialization of the new contract (if necessary)
+    function deployProxyWithImplementation(bytes32 id, address implementation, bytes memory initializeCalldata) external;
+
+    /// @dev Initializes the manager and sets necessary roles
+    function initialize() external;
+
+    /// @dev Locks immutably a link between an address and an id
+    /// @param id Hashed identifier linked to the proxy contract
+    function lock(bytes32 id) external;
+
+    /// @dev Returns the address linked to a hashed identifier
+    /// @param id Hashed identifier
+    /// @return addr Address linked to id
+    function get(
+        bytes32 id
+    ) external view returns ( address addr );
+
+    /// @dev Returns the hashed identifier linked to an address
+    /// @param addr Address
+    /// @return id Hashed identifier linked to addr
+    function idOf(
+        address addr
+    ) external view returns ( bytes32 id );
+
+    /// @dev Sets a link between a hashed identifier and an address
+    /// @param id Hashed identifier
+    /// @param addr Address
+    function setId(
+        bytes32 id,
+        address addr
+    ) external;
+
+    /// @dev Upgrades a proxy contract with an existing implementation 
+    /// @param id Hashed identifier linked to the proxy contract
+    /// @param implementation Address of the existing implementation contract
+    /// @param initializeCalldata Calldata for the initialization of the new contract (if necessary)
+    function upgrade(bytes32 id, address implementation, bytes memory initializeCalldata) external;
 }
--- contracts/v2/TutellusManager.sol
+++ contracts/v2/TutellusManager.sol
@@ -1,44 +1,26 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.9;
 
+import "contracts/interfaces/ITutellusManager.sol";
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
-contract TutellusManager is AccessControlUpgradeable {
+/// @title The contract of TutellusManager
+/// @dev Manages smart contracts deployments, ids and protocol roles
+contract TutellusManager is ITutellusManager, AccessControlUpgradeable {
+
+    /** STORAGE */
     
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
     mapping(bytes32=>address) public get;
-    mapping(address=>bytes32) public name;
     mapping(bytes32=>bool) public locked;
+    mapping(address=>bytes32) public idOf;
 
-    event NewId(bytes32 indexed id, address addr);
-    event Deployment(bytes32 indexed id, address indexed proxy, address implementation, bool upgrade);
-    event Locked(bytes32 indexed id, address addr);
+    /** PUBLIC METHODS */
 
-    function initialize() public initializer {
-        __AccessControl_init();
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(DEFAULT_ADMIN_ROLE, address(this));
-        _setupRole(UPGRADER_ROLE, address(this));
-    }
-
-    function setId(bytes32 id, address addr) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        require(!locked[id], "TutellusManager: id locked");
-        get[id] = addr;
-        name[addr] = id;
-
-        emit NewId(id, addr);
-    }
-
-    function deployProxyWithImplementation(bytes32 id, address implementation, bytes memory initializeCalldata) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        require(!locked[id], "TutellusManager: id locked");
-        _deployProxy(id, implementation, initializeCalldata);
-
-        emit Deployment(id, get[id], implementation, false);
-    }
-
+    /// @inheritdoc ITutellusManager
     function deploy(bytes32 id, bytes memory bytecode, bytes memory initializeCalldata) public onlyRole(DEFAULT_ADMIN_ROLE) returns(address implementation) {
         bool upgrading;
         assembly {
@@ -57,6 +39,38 @@ contract TutellusManager is AccessControlUpgradeable {
         emit Deployment(id, get[id], implementation, upgrading);
     }
 
+    /// @inheritdoc ITutellusManager
+    function deployProxyWithImplementation(bytes32 id, address implementation, bytes memory initializeCalldata) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(!locked[id], "TutellusManager: id locked");
+        _deployProxy(id, implementation, initializeCalldata);
+
+        emit Deployment(id, get[id], implementation, false);
+    }
+
+    /// @inheritdoc ITutellusManager
+    function initialize() public initializer {
+        __AccessControl_init();
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(DEFAULT_ADMIN_ROLE, address(this));
+        _setupRole(UPGRADER_ROLE, address(this));
+    }
+
+    /// @inheritdoc ITutellusManager
+    function lock(bytes32 id) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        locked[id] = true;
+        emit Locked(id, get[id]);
+    }
+
+    /// @inheritdoc ITutellusManager
+    function setId(bytes32 id, address addr) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(!locked[id], "TutellusManager: id locked");
+        get[id] = addr;
+        idOf[addr] = id;
+
+        emit NewId(id, addr);
+    }
+
+    /// @inheritdoc ITutellusManager
     function upgrade(bytes32 id, address implementation, bytes memory initializeCalldata) public onlyRole(DEFAULT_ADMIN_ROLE) {
         UUPSUpgradeable proxy = UUPSUpgradeable(payable(get[id]));
         if (initializeCalldata.length > 0) {
@@ -66,17 +80,15 @@ contract TutellusManager is AccessControlUpgradeable {
         }
     }
 
+    /** PRIVATE METHODS */
+
     function _deployProxy(bytes32 id, address implementation, bytes memory initializeCalldata) private {
         ERC1967Proxy proxy = new ERC1967Proxy(
             implementation,
             initializeCalldata
         );
         get[id] = address(proxy);
-        name[address(proxy)] = id;
+        idOf[address(proxy)] = id;
     }
 
-    function lock(bytes32 id) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        locked[id] = true;
-        emit Locked(id, get[id]);
-    }
-} 
+}
