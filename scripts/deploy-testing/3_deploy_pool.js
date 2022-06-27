3071989f739b87ecf742f3f5626a24acc0788e75
gperezalba <gperezalba94@gmail.com>
2022-06-27 12:50:52 +0200

    chore(deploy_pool.js) use wbtc w/ 8 decimals

--- scripts/deploy-testing/3_deploy_pool.js
+++ scripts/deploy-testing/3_deploy_pool.js
@@ -2,7 +2,7 @@ const { ethers } = require('hardhat')
 const bre = require('hardhat')
 
 const TUT_AMOUNT = ethers.utils.parseEther('400000')
-const WBTC_AMOUNT = ethers.utils.parseEther('100000')
+const WBTC_AMOUNT = '300000000'
 const TUT_ADDRESS = '0x0975234abB45394582299bB3a8fC50F1903C1ac2'
 const ROLE_MANAGER_ADDRESS = '0xF182F7576867D6516C280aacbE99c8230250C153'
 const VAULT_ADDRESS = '0xC44d907d61fB0363Cd96D628Cad6c9748976F717'
@@ -10,12 +10,12 @@ const VAULT_ADDRESS = '0xC44d907d61fB0363Cd96D628Cad6c9748976F717'
 async function main () {
     const signers = await ethers.getSigners()
 
-    const Token = await ethers.getContractFactory('Token')
+    const WBTC = await ethers.getContractFactory('WBTC')
     const TutellusFarming = await ethers.getContractFactory('TutellusFarming')
     const myUniswapFactory = await ethers.getContractAt('UniswapV2Factory', '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f')
     const myRouter = await ethers.getContractAt('UniswapRouter', '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D')
 
-    const myWBTC = await Token.deploy('Wrapped Bitcoin', 'WBTC')
+    const myWBTC = await WBTC.deploy('Wrapped Bitcoin', 'WBTC')
     await myWBTC.deployed()
     const approvalbtc = await myWBTC.approve(myRouter.address, ethers.constants.MaxUint256)
     await approvalbtc.wait()
