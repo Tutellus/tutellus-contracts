0d795e83987fad5b7a2eab3adc587b1cbfe616aa
gperezalba <gperezalba94@gmail.com>
2022-06-27 12:51:13 +0200

    feat(WBTC) token with 8 decimals

---
+++ contracts/utils/WBTC.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+
+contract WBTC is ERC20Upgradeable {
+
+    address public owner;
+
+    constructor (string memory name_, string memory symbol_) initializer {
+        __ERC20_init(name_, symbol_);
+        owner = msg.sender;
+        _mint(owner, 1e28);
+    }
+
+    function mint(address account, uint256 amount) public {
+        require(msg.sender == owner, "Token: only owner can mint");
+        _mint(account, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 8;
+    }
+}
