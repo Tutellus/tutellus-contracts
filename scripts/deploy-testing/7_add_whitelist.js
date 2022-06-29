9ed56dec627f5dd544a684f796b2caba4ebab5cf
gperezalba <gperezalba94@gmail.com>
2022-06-29 08:26:56 +0200

    chore(add_whitelist) choose addrs to add

--- scripts/deploy-testing/7_add_whitelist.js
+++ scripts/deploy-testing/7_add_whitelist.js
@@ -3,6 +3,13 @@ const { downloadJSON, uploadJSON } = require('../../utils/ipfs');
 const { parseEther } = require('ethers/lib/utils');
 const { concatArrays, getWhitelistTree } = require('../../utils/whitelistTree');
 
+const ADD_ADDRESSES = [
+    "0xCD7669AAFffB7F683995E6eD9b53d1E5FE72c142",
+    "0xCC18024a12FcF2099693cB6C22eb127765Ae6dbf",
+    "0x0DD1AC51cBaDD3e4DFa98DdD07E383d4706b7732",
+    "0xfF529C047ecFe2F78cAF41E84773C4f865eF84F1"
+];
+
 const main = async () => {
     await hre.run('compile');
 
@@ -19,7 +26,7 @@ const main = async () => {
         const address = currentAddresses[i]
         const claim = tree.claims[address];
         const isWhitelisted = await myWhitelist.whitelisted(address);
-        if (!isWhitelisted) {
+        if (!isWhitelisted && ADD_ADDRESSES.includes(address)) {
             console.log('Adding account:', address);
             const tx = await myWhitelist.add(
                 claim.index,
