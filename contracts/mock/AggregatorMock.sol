70823c366399598b5882cb371d757010eda4000d
gperezalba <gperezalba94@gmail.com>
2022-08-23 10:58:36 +0200

    chore(S2L) beforeEach tests

---
+++ contracts/mock/AggregatorMock.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract AggregatorMock is AggregatorV3Interface, Initializable {
+    uint80 private _round = 0;
+    uint256 private _time = 0;
+    string private _description = "mock";
+    uint8 private _decimals;
+    int256 private _answer;
+
+    function initialize(uint8 decimals_, int256 answer) public initializer {
+        __AggregatorMock_init(decimals_, answer);
+    }
+
+    // solhint-disable-next-line
+    function __AggregatorMock_init(uint8 decimals_, int256 answer)
+        internal
+    {
+        _decimals = decimals_;
+        _answer = answer;
+    }
+
+    function decimals() external view override returns (uint8) {
+        return _decimals;
+    }
+
+    function description() external view override returns (string memory) {
+        return _description;
+    }
+
+    function version() external view override returns (uint256) {
+        return _time;
+    }
+
+    function latestRoundData()
+        external
+        view
+        override
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
+        return (_round, _answer, _time, _time, _round);
+    }
+
+    function getRoundData(uint80 _roundId)
+        external
+        view
+        override
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
+        return (_roundId, _answer, _time, _time, _round);
+    }
+}
--- test/v2/stakeToLearn/StakeToLearn.test.js
+++ test/v2/stakeToLearn/StakeToLearn.test.js
@@ -18,8 +18,11 @@ const HoldersVault = artifacts.require('TutellusHoldersVault')
 
 const STAKETOLEARN_FACTORY = ethers.utils.id("STAKETOLEARN_FACTORY")
 
-const ONE_ETHER = ether('1')
-const TWO_ETHER = ether('2')
+const FEED_BTC_USD = ethers.utils.id('FEED_BTC_USD')
+const FEED_EUR_USD = ethers.utils.id('FEED_EUR_USD')
+
+const BTC_USD = '2376469736217'
+const EUR_USD = '108091000'
 
 let myDeployer
 let myManager
@@ -27,6 +30,8 @@ let myToken
 let myRolemanager
 let myRewardsVault
 let myHoldersVault
+let myStaking
+let myFarming
 let owner, person
 
 const getAddresses = async () => {
@@ -66,8 +71,20 @@ describe('StakeToLearn', function () {
 		myManager = await Manager.deploy()
 		await myManager.initialize()
 
+		myStaking = await Staking.new(myToken.address, myRolemanager.address, myRewardsVault.address, ether('0.1'), ether('10'), 10)
+		myFarming = await Farming.new(myToken.address, myRolemanager.address, myRewardsVault.address)
+		await myRewardsVault.add(myStaking.address, [ether('100')])
+		await myRewardsVault.add(myFarming.address, [ether('20'), ether('80')])
+
+        const AggregatorMock = await hre.ethers.getContractFactory('AggregatorMock')
+		const btcUsdCalldata = AggregatorMock.interface.encodeFunctionData('initialize', ['8', BTC_USD]);
+        const eurUsdCalldata = AggregatorMock.interface.encodeFunctionData('initialize', ['8', EUR_USD]);
+        await myManager.deploy(FEED_BTC_USD, AggregatorMock.bytecode, btcUsdCalldata)
+        await myManager.deploy(FEED_EUR_USD, AggregatorMock.bytecode, eurUsdCalldata)
+
         const TutellusStakeToLearnFactory = await ethers.getContractFactory('TutellusStakeToLearnFactory')
         const initializeCalldata = TutellusStakeToLearnFactory.interface.encodeFunctionData('initialize', [
+			myStaking.address,
 
 		])
         await myManager.deploy(STAKETOLEARN_FACTORY, TutellusStakeToLearnFactory.bytecode, initializeCalldata)
@@ -75,10 +92,7 @@ describe('StakeToLearn', function () {
 	describe('Deposit', () => {
 		let myStaking, myFarming
 		beforeEach(async () => {
-			myStaking = await Staking.new(myToken.address, myRolemanager.address, myRewardsVault.address, ether('0.1'), ether('10'), 10)
-			myFarming = await Farming.new(myToken.address, myRolemanager.address, myRewardsVault.address)
-			await myRewardsVault.add(myStaking.address, [ether('100')])
-			await myRewardsVault.add(myFarming.address, [ether('20'), ether('80')])
+			
 		})
 		it('Deposit claimed tokens from holders Vault', async () => {
 			await myHoldersVault.claim({ from: person })
