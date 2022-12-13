cf3de437b5fb351da8436c55908fae72d9357ba3
victormer <victormerinokosina@protonmail.com>
2022-12-13 13:53:03 +0100

    upgrade hardhat

--- scripts/testnet/manager/deploySingle.js
+++ scripts/testnet/manager/deploySingle.js
@@ -1,12 +1,8 @@
-const bre = require('hardhat');
-const ethers = bre.ethers;
-const { utils, constants, Wallet, provider } = ethers;
-const { createTx, sendTx } = require('../../../utils/gnosis');
+const { ethers } = require('hardhat');
+const { utils, constants } = ethers;
 
-const ID = utils.id('POAP');
-const CONTRACT_NAME = 'TutellusPOAP';
-
-const SAFE = "0x5ACB3043da168b59b775eA28F3942597F45e9543"
+const ID = utils.id('CLIENTS_VAULT');
+const CONTRACT_NAME = 'TutellusClientsVaultV2';
 
 const INITIALIZE = {
   sig: 'initialize(string)',
@@ -14,47 +10,33 @@ const INITIALIZE = {
 }
 
 async function main () {
-  bre.run('compile');
-  const Manager = await ethers.getContractFactory('TutellusManager');
-  const myManager = Manager.attach('0x0e75e4D2041287813a693971634400EAe765910C');
-
-  const wallet = new Wallet(process.env.PRIVATE_KEY);
-
-  const proxyCheck = await myManager.get(ID)
-
-  console.log('proxyCheck', proxyCheck)
-  // const Contract = await ethers.getContractFactory(CONTRACT_NAME);
+  const myManager = await ethers.getContractAt('TutellusManager', '0x0e75e4D2041287813a693971634400EAe765910C');
+  const proxyCheck = await myManager.get(ID);
+  const Contract = await ethers.getContractFactory(CONTRACT_NAME);
   
-  // let initializeCalldata;
+  const provider = ethers.provider;
 
-  // if(proxyCheck == constants.AddressZero) {
-  //   console.log('Deploying...')
-  //   initializeCalldata = Contract.interface.encodeFunctionData(INITIALIZE.sig, INITIALIZE.args);
-  // } else {
-  //   console.log('Upgrading...')
-  //   initializeCalldata = '0x';
-  // }
+  console.log('Provider', provider)
 
-  // console.log('Deploying...')
+  let initializeCalldata;
+
+  if(proxyCheck == constants.AddressZero) {
+    console.log('Deploying...')
+    initializeCalldata = Contract.interface.encodeFunctionData(INITIALIZE.sig, INITIALIZE.args);
+  } else {
+    console.log('Upgrading...')
+    initializeCalldata = '0x';
+  }
+
   // const deploying = await myManager.deploy(ID, Contract.bytecode, initializeCalldata);
   // await deploying.wait();
-  // console.log('Deployed', deploying.hash)
 
-  // const data = {
-  //   to: myManager.address,
-  //   data: myManager.interface.encodeFunctionData('deploy', [
-  //     ID,
-  //     Contract.bytecode,
-  //     initializeCalldata,
-  //   ]),
-  //   value: 0,
-  //   operation: 0,
-  // };
+  // const proxy = await myManager.get(ID);
+  // console.log('Proxy deployed at', proxy);
 
-  // const chainId = provider._network.chainId;
-  // const txData = await createTx(provider, chainId, SAFE, data, wallet);
-  // await sendTx(chainId, SAFE, txData);
-  // console.log('SafeTxHash:', txData.contractTransactionHash)
+  // const implementation = await myManager.implementationByProxy(proxy);
+  // console.log('Implementation deployed at', implementation.address);
+  // console.log('hardhat verify --network goerli', implementation.address);
 }
 
 main()
